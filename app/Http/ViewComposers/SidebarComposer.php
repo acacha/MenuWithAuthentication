38f707fb41f2e85f2b863589aca4fcba92a426ff
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2015-12-14 19:57:34 +0100

    MenuItem

---
+++ app/Http/ViewComposers/SidebarComposer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MenuWithAuthentication\Http\ViewComposers;
+
+use Illuminate\Contracts\View\View;
+use MenuWithAuthentication\MenuWithAuthentication;
+
+
+/**
+ * Class SidebarComposer
+ * @package MenuWithAuthentication\Http
+ */
+class SidebarComposer
+{
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('menu', $this->getSideBarMenu());
+    }
+
+    private function getSideBarMenu()
+    {
+        $menu =
+            MenuWithAuthentication::instance()
+                ->getMenu();
+        return array($menu);
+    }
+}
---
+++ app/Menu/MenuItem.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace MenuWithAuthentication\Menu;
+
+
+/**
+ * Class MenuItem
+ * @package MenuWithAuthentication\Menu
+ */
+/**
+ * Class MenuItem
+ * @package MenuWithAuthentication\Menu
+ */
+class MenuItem
+{
+    /**
+     * @var
+     */
+    protected $title;
+
+    /**
+     * @var
+     */
+    protected $role;
+    /**
+     * @var
+     */
+    protected $icon = null;
+    /**
+     * @var
+     */
+    protected $url = null;
+    /**
+     * @var
+     */
+    protected $permission;
+    /**
+     * @var
+     */
+    protected $user;
+
+    /**
+     * @var
+     */
+    private $id;
+
+    /**
+     * MenuItem constructor.
+     */
+    public function __construct($id)
+    {
+
+        $this->id = $id;
+    }
+
+    /**
+     * @param null $title
+     * @return $this
+     */
+    public function title($title=null)
+    {
+        if ($title == null) {
+            return $this->title;
+        }
+
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @param null $icon
+     * @return $this
+     */
+    public function icon($icon=null)
+    {
+        if ($icon == null) {
+            return $this->icon;
+        }
+
+        $this->icon = $icon;
+        return $this;
+    }
+
+    /**
+     * @param null $url
+     * @return $this
+     */
+    public function url($url=null)
+    {
+        if ($url == null) {
+            return $this->url;
+        }
+
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @param null $role
+     * @return $this
+     */
+    public function role($role=null)
+    {
+        if ($role == null) {
+            return $this->role;
+        }
+
+        $this->role = $role;
+        return $this;
+    }
+
+    /**
+     * @param null $permission
+     * @return $this
+     */
+    public function permission($permission=null)
+    {
+        if ($permission == null) {
+            return $this->permission;
+        }
+
+        $this->permission = $permission;
+        return $this;
+    }
+
+    /**
+     * @param null $user
+     * @return $this
+     */
+    public function user($user=null)
+    {
+        if ($user == null) {
+            return $this->user;
+        }
+
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->render();
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        $data = array();
+        $data['url'] = $this->url;
+        $data['icon'] = $this->icon;
+        $data['title'] = $this->title;
+        $data['id'] = $this->id;
+//        $data['permission'] = $this->permission;
+//        $data['role'] = $this->role;
+//        $data['user'] = $this->user;
+
+        return (String) view('menu.menuitem',$data);
+    }
+}
